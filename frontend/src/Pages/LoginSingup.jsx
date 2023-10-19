42e8c74ab9815c30c87faba4cb1f508f44db9ecc
Ajay Kumar Gupta <rg190559@gmail.com>
2023-10-19 21:27:17 +0530

    create desing of login and singup page

--- frontend/src/Pages/LoginSingup.jsx
+++ frontend/src/Pages/LoginSingup.jsx
@@ -1,5 +1,26 @@
+import "./CSS/LoginSingup.css";
+
 const LoginSingup = () => {
-  return <div></div>;
+  return (
+    <div className="loginsingup">
+      <div className="loginsingup-container">
+        <h1>Sing Up</h1>
+        <div className="loginsingup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsingup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsingup-agree">
+          <input type="checkbox" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSingup;
